b9197a7204a1fe7e3f0edd1d92d90c259d6f549f
Thomas <32342015+Atoms-Github@users.noreply.github.com>
2022-10-19 17:11:34 +0100

    Minimized tests, to only include one

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,33 +1,6 @@
 use criterion::*;
 use ecs_bench_suite::*;
 
-fn bench_simple_insert(c: &mut Criterion) {
-    let mut group = c.benchmark_group("simple_insert");
-    group.bench_function("legion", |b| {
-        let mut bench = legion::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("bevy", |b| {
-        let mut bench = bevy::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("hecs", |b| {
-        let mut bench = hecs::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("planck_ecs", |b| {
-        let mut bench = planck_ecs::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("shipyard", |b| {
-        let mut bench = shipyard::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("specs", |b| {
-        let mut bench = specs::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-}
 
 fn bench_simple_iter(c: &mut Criterion) {
     let mut group = c.benchmark_group("simple_iter");
@@ -205,15 +178,51 @@ fn bench_serialize_binary(c: &mut Criterion) {
     // });
 }
 
+fn bench_simple_insert(c: &mut Criterion) {
+    let mut group = c.benchmark_group("simple_insert");
+    group.bench_function("legion", |b| {
+        let mut bench = legion::simple_insert::Benchmark::new();
+        b.iter(move || bench.run());
+    });
+    group.bench_function("bevy", |b| {
+        let mut bench = bevy::simple_insert::Benchmark::new();
+        b.iter(move || bench.run());
+    });
+    group.bench_function("hecs", |b| {
+        let mut bench = hecs::simple_insert::Benchmark::new();
+        b.iter(move || bench.run());
+    });
+    group.bench_function("planck_ecs", |b| {
+        let mut bench = planck_ecs::simple_insert::Benchmark::new();
+        b.iter(move || bench.run());
+    });
+    group.bench_function("shipyard", |b| {
+        let mut bench = shipyard::simple_insert::Benchmark::new();
+        b.iter(move || bench.run());
+    });
+    group.bench_function("specs", |b| {
+        let mut bench = specs::simple_insert::Benchmark::new();
+        b.iter(move || bench.run());
+    });
+}
+
+fn super_simple_insert(c: &mut Criterion) {
+    let mut group = c.benchmark_group("simple_insert");
+    group.bench_function("bevy", |b| {
+        let mut bench = bevy::simple_insert::Benchmark::new();
+        b.iter(move || bench.run());
+    });
+}
 criterion_group!(
     benchmarks,
-    bench_simple_insert,
-    bench_simple_iter,
-    bench_frag_iter_bc,
-    bench_schedule,
-    bench_heavy_compute,
-    bench_add_remove,
-    bench_serialize_text,
-    bench_serialize_binary,
+    super_simple_insert,
+    // bench_simple_iter,
+    // bench_frag_iter_bc,
+    // bench_schedule,
+    // bench_heavy_compute,
+    // bench_add_remove,
+    // bench_serialize_text,
+    // bench_serialize_binary,
 );
 criterion_main!(benchmarks);
+
